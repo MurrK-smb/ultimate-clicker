d9f50f8c813327eb95b43ef6ae1fa8520f534e96
NicolasGil <nick.gil360@gmail.com>
2022-12-28 17:03:46 -0500

    add gamejs and gamecss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Navbar.js";
+import Game from "./components/Game.js";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Game />
     </div>
   );
 }
---
+++ src/components/Game.js
@@ -0,0 +1,8 @@
+import "./Game.css";
+import React from "react";
+
+function Game() {
+  return <main className="game"></main>;
+}
+
+export default Game;
