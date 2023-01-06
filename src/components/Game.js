6cc9c431e267db3a5749c2d5c62be5c3eb0abd43
NicolasGil <nick.gil360@gmail.com>
2023-01-06 06:04:56 -0500

    create counter button

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,7 +4,9 @@ import React from "react";
 function Game() {
   return (
     <div className="game">
-      <p className="game__counter">0</p>
+      <button className="game__button">
+        <p className="game__counter">0</p>
+      </button>
     </div>
   );
 }
