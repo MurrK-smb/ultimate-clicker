bf10a8a494766fb6714e6ca8a8c62861ba3bf34c
NicolasGil <nick.gil360@gmail.com>
2023-01-22 15:18:54 -0500

    import Route, Switch}

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Navbar from "./Navbar/Navbar.js";
 import Game from "./Game/Game.js";
-import { BrowserRouter as Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
