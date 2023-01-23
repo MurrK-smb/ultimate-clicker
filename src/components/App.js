e0a3a9c00f451abebd5e7c14c924604df43907f8
NicolasGil <nick.gil360@gmail.com>
2023-01-23 06:42:31 -0500

    create backend folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import Navbar from "./Navbar/Navbar.js";
 import Game from "./Game/Game.js";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
