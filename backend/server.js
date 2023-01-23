280fd9cdaec09d737b4b580b5dbd02d50e417286
NicolasGil <nick.gil360@gmail.com>
2023-01-23 06:53:48 -0500

    add server boilerplate

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,14 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`app is running on port ${port}`);
+});
